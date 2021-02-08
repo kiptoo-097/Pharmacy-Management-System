8ef50c26e6492d043da65ae9e3b44a4e7e6d86e8
kiptoo-097 <kiptoo097@gmail.c0m>
2021-02-08 17:36:49 +0300

    Added views that render page

--- pharmacy_projct/products/views.py
+++ pharmacy_projct/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from product_app.models import Product
 
-# Create your views here.
+
+def home(request):
+    products = Product.objects.all().order_by('-id')
+
+    return render(request,
+                  'products/index.html',
+                  {'products': products})
